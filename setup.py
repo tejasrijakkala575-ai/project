dd92d77058e15d9e8169ce77781085f55062da00
Jakkala Tejasri <tejasri123@gmail.com>
2026-04-08 09:51:17 +0530

    Updated exception, logger and setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="qasystem",
+    version="0.0.1",
+    author="tej",
+    author_email="tejasrijakkala575@gmail.com",
+    packages=find_packages(),
+    install_requires=["langchain"]
+)
